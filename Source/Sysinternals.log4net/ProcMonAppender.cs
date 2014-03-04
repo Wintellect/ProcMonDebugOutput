e3f0262db430345ba5bd5911951906fccf3df9a7
Justin Dearing <zippy1981@gmail.com>
2014-03-03 20:27:00 -0500

    added a log4net appender

---
+++ Source/Sysinternals.log4net/ProcMonAppender.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using log4net.Appender;
+using log4net.Core;
+using log4net.Layout;
+using Sysinternals.Debug;
+
+namespace Sysinternals.log4net
+{
+    /// <summary>
+    /// A <c>log4net</c> appender for ProcMonDebugOutput.
+    /// </summary>
+    public class ProcMonAppender : AppenderSkeleton
+    {
+        /// <summary>
+        ///  Default constructor.
+        /// </summary>
+        /// <remarks>
+        /// Sets the default layout.
+        /// </remarks>
+        public ProcMonAppender()
+        {
+            // Although it breaks convention set by the built-in appenders, this is more forgiving.
+            Layout = new PatternLayout("%-5p %m");
+        }
+        /// <summary>
+        /// This appender requires a <see cref="AppenderSkeleton.Layout"/> to be set.
+        /// </summary>
+        /// <value><c>true</c></value>
+        override protected bool RequiresLayout
+        {
+            get { return true; }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="loggingEvent"></param>
+        protected override void Append(LoggingEvent loggingEvent)
+        {
+            NativeMethods.ProcMonDebugOutput(RenderLoggingEvent(loggingEvent));
+        }
+    }
+}
